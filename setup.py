e5067bc04d199ae0c5dad4e9fd546eabeadf0db8
SumitRoy-Gh <sumitr82324@gmail.com>
2026-05-02 02:14:36 +0530

    updated requirements,installed requirements,imported trained model

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup 
+
+setup(
+    name = "Sign-Language-end-to-end-object-detection",
+    version = "0.0.0",
+    author="Sumit",
+    author_email="sumitr82324@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
